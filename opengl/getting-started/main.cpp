3061c6fb7bd0c6e88c9a46d65c946f105daebc65
scimad <scimad2.71@gmail.com>
2022-03-23 01:50:34 +0545

    Finally an OpenGL window loads properly

--- opengl/getting-started/main.cpp
+++ opengl/getting-started/main.cpp
@@ -1,15 +1,47 @@
+#include "glad/glad.h"
+
 #include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <string>
 
-int main()
-{
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 
-    std::cout << "[ZR]: This works!" << std::endl;
+
+int main(){
+    if(!glfwInit()){
+        std::cout << "GLFW initialization failed." << std::endl;
+        return -1;
+    }
+    std::cout << "Successfully initialized GLFW." << std::endl;
+
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "ZR :: OpenGL", NULL, NULL);
+    if (window == NULL){
+        std::cout << "Failed to create GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    std::cout << "Successfully created GLFW window." << std::endl;
+
+    glfwMakeContextCurrent( window );
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    std::cout << "Successfully initialized GLAD window." << std::endl;
+
+    while (!glfwWindowShouldClose(window)){
+        processInput(window);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    
+    glfwTerminate();
     return 0;
 }
