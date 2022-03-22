3496b64616a05d2038ce7e2dc604c03b7fcce399
scimad <scimad2.71@gmail.com>
2022-03-23 01:11:44 +0545

    Add OpenGL project template code

---
+++ opengl/getting-started/README.md
@@ -0,0 +1,3 @@
+# Starter Template for OpenGL - GLFW programs
+
+(Have not included <glad/glad.h>)
---
+++ opengl/getting-started/main.cpp
@@ -0,0 +1,18 @@
+// #include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    std::cout << "[ZR]: This works!" << std::endl;
+    return 0;
+}
