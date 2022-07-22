6c05bca482b63b52e7b85d4b37342312526cb059
scimad <scimad2.71@gmail.com>
2022-07-22 16:07:27 +0545

    Add The Cherno YT | Add lambdas example

---
+++ cpp-basic/cherno-yt/lambdas/lambdas_demo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+    int a = 10;
+    int b = 20;
+
+    auto lambda_func = [&a, b](){
+        a = a * 10;
+        return (a + b)/2;
+    };
+
+    std::cout << lambda_func()<< std::endl;
+
+    a+=100;
+    b+=200;
+    std::cout << lambda_func()<< std::endl;
+
+    std::cout << "a = " << a << " and b = " << b << std::endl;
+
+}
