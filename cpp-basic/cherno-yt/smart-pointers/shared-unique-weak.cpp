6cfeadf08f4bba60cd71656ffb93284896164d91
Madhav Humagain <scimad2.71@gmail.com>
2022-08-11 11:44:42 +0545

    Add smart poitners example

---
+++ cpp-basic/cherno-yt/smart-pointers/shared-unique-weak.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <memory>
+
+class TestClass
+{
+private:
+    std::string my_name;
+public:
+    TestClass(std::string name):my_name(name)
+    {
+        std::cout << "Object created with name " << my_name << std::endl;
+    };
+    ~TestClass()
+    {
+        std::cout << "Destroyed object with name " << my_name << std::endl;
+    };
+    void setName(std::string name){
+        std::cout << "Setting name: " << name << std::endl;
+
+        if (this)
+        {
+        std::cout << "Name changed from " << my_name << " to " << name << std::endl;
+        my_name = name;
+        }else{
+            std::cout << "Can't write the name \"" << name <<  "\" as the object you're trying to write into is already dead." << std::endl;
+        }
+
+    }
+};
+
+
+int main(int argc, char const *argv[])
+{
+    {
+        std::shared_ptr<TestClass> s_ptr;
+        {
+            auto s2_ptr = std::make_shared<TestClass>("Madhav"); //returns std::shared_ptr
+            s2_ptr->setName("Ram");
+            auto s_ptr = s2_ptr;
+        }
+        s_ptr->setName("Hari");
+    }
+
+    {
+        std::unique_ptr<TestClass> u_ptr;
+        {
+            auto u2_ptr = std::make_unique<TestClass>("Apple");
+            u_ptr = std::make_unique<TestClass>("Ball");
+        }
+        u_ptr->setName("Cat");
+    }
+
+    {
+        auto s_ptr= std::make_shared<TestClass>("Kapuri");
+        std::weak_ptr<TestClass> w_ptr = s_ptr; //w_ptr doesn't take ownership of Kapuri
+        s_ptr->setName("Kharayo");
+        std::cout << "Ref count = " << s_ptr.use_count() << std::endl;
+
+        {
+            auto p = w_ptr.lock();  //p takes the ownership of Kapuri, ref count is increased
+            std::cout << "Ref count = " << s_ptr.use_count() << std::endl;
+        }   //p is destroyed and refcount of Kapuri (now Kharayo) is decreased
+
+        std::cout << "Ref count = " << s_ptr.use_count() << std::endl;
+    }
+}
