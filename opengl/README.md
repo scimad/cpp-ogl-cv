e933956f2cd0c17f47ca723f3dbb3026974d4ddf
scimad <scimad2.71@gmail.com>
2022-08-12 21:49:43 +0545

    Initial Commit for Cherno OpenGL

---
+++ opengl/README.md
@@ -0,0 +1,5 @@
+# The Cherno - OpenG series
+
+Few notes:
+
+* OpenGL_GL_PREFERENCE has been set to "GLVND"
---
+++ opengl/cherno-opengl/01-windowing/windowing.cpp
@@ -0,0 +1,46 @@
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <string>
+
+#include <ZR/core.hpp>
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+int main() {
+  if (!glfwInit()) {
+    zr::log("GLFW initialization failed.");
+    return -1;
+  }
+  zr::log("Successfully initialized GLFW.");
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "ZR :: Cherno-OpenGL", NULL, NULL);
+  if (window == NULL) {
+    zr::log("Failed to create GLFW window.");
+    glfwTerminate();
+    return -1;
+  }
+  zr::log("Successfully created GLFW window.");
+
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    zr::log("Failed to initialize GLAD");
+    return -1;
+  }
+  zr::log("Successfully initialized GLAD window.");
+
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
