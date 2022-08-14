3db2eb84b2088c7292a85ba38db8a81e5dae3838
scimad <scimad2.71@gmail.com>
2022-08-14 22:06:05 +0545

    Add multiple window

--- opengl/cherno-opengl/01-windowing/windowing.cpp
+++ opengl/cherno-opengl/01-windowing/windowing.cpp
@@ -20,6 +20,22 @@ int main() {
   zr::log("Successfully initialized GLFW.");
 
   GLFWwindow *window = glfwCreateWindow(800, 600, "ZR :: Cherno-OpenGL", NULL, NULL);
+  GLFWwindow *window2 = glfwCreateWindow(600, 800, "ZR :: OpenGL-Cherno", NULL, NULL);
+
+  if (!window || !window2)
+  {
+      glfwTerminate();
+      return -1;
+  }
+
+    /* Make the window's context current */
+    // glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    static bool exit_flag = false;
+    while (!exit_flag)
+    {
+
   if (window == NULL) {
     zr::log("Failed to create GLFW window.");
     glfwTerminate();
@@ -36,6 +52,27 @@ int main() {
 
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
+    exit_flag = exit_flag || glfwWindowShouldClose(window) || glfwWindowShouldClose(window2);;
+    /* Render here */
+    glfwMakeContextCurrent(window2);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(0, 1);
+    glVertex2f(1, 0);
+    glVertex2f(0, 0);
+    glEnd();
+    glfwSwapBuffers(window2);
+    /* Swap front and back buffers */
+
+    glfwMakeContextCurrent(window);
+    /* Render here */
+    glClearColor(1.0, 0, 0, 0.5);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+    glVertex2f(0, -1);
+    glVertex2f(-1, 0);
+    glVertex2f(0, 0);
+    glEnd();
 
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -43,4 +80,6 @@ int main() {
 
   glfwTerminate();
   return 0;
+}
+
 }
