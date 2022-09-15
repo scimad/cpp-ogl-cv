54dddb2d69f621cb9f93352dd46bd24803222105
scimad <scimad2.71@gmail.com>
2022-09-15 22:14:45 +0545

    Add basic example (a not-so-good one)

--- cpp-basic/src/cherno-yt/multithreading/file-copy.cpp
+++ cpp-basic/src/cherno-yt/multithreading/file-copy.cpp
@@ -4,8 +4,31 @@
 #include <filesystem>
 #include <cctype>
 #include <future>
+#include <chrono>
+#include <any>
 
-void read_and_rewrite_txt(std::string file_path){
+class SimpleTimer
+{
+public:
+    std::chrono::time_point<std::chrono::high_resolution_clock> begin, end;
+    SimpleTimer(/* args */){
+        begin = std::chrono::high_resolution_clock::now();
+    };
+    ~SimpleTimer(){
+        std::chrono::duration<int, std::micro> d;
+        d = get_duration();
+        std::cout << "The timer lived for " << d.count() << " micro." << std::endl;
+    };
+
+    std::chrono::duration<int, std::micro> get_duration(){
+        return std::chrono::duration_cast<std::chrono::duration<int, std::micro>>(std::chrono::high_resolution_clock::now()-begin);
+    }
+
+};
+
+
+bool read_and_rewrite_txt(std::string file_path){
+    bool scrambled = false;
     if (std::filesystem::is_regular_file(file_path)){
         std::string ext = file_path.substr(file_path.find_last_of('.'),4);
         //lambda to convert string to lower
@@ -14,7 +37,6 @@ void read_and_rewrite_txt(std::string file_path){
                 s = tolower(s);
         }(ext);
         if (ext.compare(".txt") == 0){
-            std::cout << ".txt file found: " << file_path << std::endl;
             std::ifstream f;
             f.open(file_path);
             while (f.is_open()){
@@ -40,15 +62,16 @@ void read_and_rewrite_txt(std::string file_path){
                 std::string filename = file_path.substr(file_path.find_last_of('/')+1);
                 
                 std::ofstream w;
-                w.open(folder_path + "scrambled_" +  filename);
+                w.open(folder_path + "scrambled_" +  filename + ".xtx");
                 if (w.is_open()){
                     w.write(new_str.c_str(),sizeof(char)*new_str.length());
                 }
                 w.close();
-                std::cout << " File scrambled " << std::endl;
+                scrambled = true;
             }
         }
     }
+    return scrambled;
 }
 
 int main(int argc, char* argv[]){
@@ -58,12 +81,33 @@ int main(int argc, char* argv[]){
     }
     
     const std::string folder_path = std::string(argv[1]);
-
-    for (const auto& entry : std::filesystem::directory_iterator(folder_path)){
-        std::string filename = entry.path();
-        read_and_rewrite_txt(filename);
-        // std::async()
-
+    {
+        int n_files = 0;
+        SimpleTimer T;
+        for (const auto& entry : std::filesystem::directory_iterator(folder_path)){
+            std::string filename = entry.path();
+            std::future<bool> success = std::async(std::launch::async, read_and_rewrite_txt, filename);
+            if (success.get()){
+                std::cout << "Scrambling " << filename << std::endl;
+                n_files++;
+            }
+        }
+        if (n_files != 0)
+            std::cout << "average time for each file = " <<  T.get_duration().count() / n_files << std::endl;//.count() / n_files;
+    }
+    std::cout << "-----------------------------" << std::endl;
+    {
+        int n_files = 0;
+        SimpleTimer T;
+        for (const auto& entry : std::filesystem::directory_iterator(folder_path)){
+            std::string filename = entry.path();
+            if (read_and_rewrite_txt(filename)){
+                std::cout << "Scrambling " << filename << std::endl;
+                n_files++;
+            }
+        }
+        if (n_files != 0)
+            std::cout << "average time for each file = " <<  T.get_duration().count() / n_files << std::endl;//.count() / n_files;
     }
     return 0;
 }
