f1fc5a5104d3fd37bfb9c3c0152b88f5ad9e431e
scimad <scimad2.71@gmail.com>
2022-09-29 23:34:10 +0545

    Add WIP | Sovle segfault on resize

--- cpp-basic/src/cherno-yt/dynamic-arrays/basic.cpp
+++ cpp-basic/src/cherno-yt/dynamic-arrays/basic.cpp
@@ -4,26 +4,26 @@
 #include <stdexcept>
 #include <cstring>
 
-// void* operator new(size_t size){
-//     void* ptr = malloc(size);
-//     std::cout << "[" << size << " Bytes] allocated on heap at location:" << ptr << std::endl;
-//     return ptr;
-// }
-
-// void operator delete(void* ptr){
-//     free(ptr);
-//     std::cout << "Memory freed from heap at location:" << ptr << std::endl;
-// }
-
-void* operator new[](size_t size){
+void* operator new(size_t size){
     void* ptr = malloc(size);
     std::cout << "[" << size << " Bytes] allocated on heap at location:" << ptr << std::endl;
     return ptr;
 }
 
-void operator delete[](void* ptr){
+void operator delete(void* ptr){
     free(ptr);
-    std::cout << "Memory freed from heap at location:" << ptr << std::endl;
+    std::cout << "delete: Memory freed from heap at location:" << ptr << std::endl;
+}
+
+void* operator new[](size_t size){
+    void* ptr = malloc(size);
+    std::cout << "operator new[]: [" << size << " Bytes] allocated on heap at location:" << ptr << std::endl;
+    return ptr;
+}
+
+void operator delete[](void* ptr, size_t size){
+    std::cout << "operator delete[]: Freeing up heap memory at location:" << ptr << std::endl;
+    free(ptr);
 }
 
 typedef std::string T;
@@ -34,12 +34,11 @@ private:
     T* resize();
     void push_back();
     T at(size_t index);
-    T *Tbuffer;
+    T* Tbuffer = nullptr;
 
 public:
-    DynamicArray(size_t size = 10)
-    {
-        std::cout << "Creating DA of size " << size << "."<< std::endl;
+    DynamicArray(size_t size = 10){
+        std::cout << "DynamicArray: Creating DA of size " << size << "."<< std::endl;
         this->capacity = size;
         this->n = size;
         // Heap Allocation
@@ -47,18 +46,20 @@ public:
     }
 
     DynamicArray(size_t size, T _default):DynamicArray(size){
-        std::cout<< "Initialized array with value: " << _default << std::endl;
+        std::cout<< "DynamicArray: Initialized array with value: " << _default << std::endl;
         for (size_t i = 0; i < size; i++){
             Tbuffer[i] = _default;
         }
     }
 
     ~DynamicArray(){
-        delete[] Tbuffer;
+        std::cout<< "~DynamicArray: Calling destructor." << std::endl;
+        delete[] Tbuffer;//, capacity;
     };
 
     //For assignment and access
     T& operator[](size_t index){
+        std::cout << " Implementation 1" << std::endl;
         if (index<n)
             return Tbuffer[index];
         else
@@ -74,21 +75,24 @@ public:
 
     // returns new location if succesful and nullptr if allocation wasn't succesful
     T* resize(size_t new_capacity){
-        T* old_Buffer = Tbuffer;
-        T* Tbuffer = new T[new_capacity];
-        for (size_t i=0; i<capacity; ++i){
-            Tbuffer[i] = old_Buffer[i];
+        std::cout << "resize: Resizing from " << capacity << " to " << new_capacity << "." << std::endl;
+
+        T* newBuffer = new T[new_capacity];
+        for (size_t i=0; i<(new_capacity?new_capacity<capacity:capacity); ++i){
+            newBuffer[i] = Tbuffer[i];
         }
-        delete[] old_Buffer;
+        capacity = new_capacity;
+        delete[] Tbuffer;
+        Tbuffer = newBuffer;
+        std::cout << "resize: Resized." << std::endl;
         return Tbuffer;
     }
 };
 
 int main()
 {
-    // DynamicArray arr(10);
     DynamicArray arr(10, "Madhav");
-    arr[3] = "Humagain";
-    std::cout << "arr[3] = " << arr[3] << std::endl;
+    // arr[3] = "Humagain";
+    // std::cout << "arr[3] = " << arr[3] << std::endl;
     arr.resize(15);
 }
