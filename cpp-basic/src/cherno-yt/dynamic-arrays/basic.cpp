20b3065e5e2223b178212db73f86ad7e67b8dcae
Madhav Humagain <scimad2.71@gmail.com>
2022-10-10 10:58:15 +0545

    Complete basic dynamic array class

--- cpp-basic/src/cherno-yt/dynamic-arrays/basic.cpp
+++ cpp-basic/src/cherno-yt/dynamic-arrays/basic.cpp
@@ -3,11 +3,38 @@
 #include "../custom-strings/mystring.hpp"
 #include "./dynaimc-array.hpp"
 
-int main(){
-    auto a = myString("Apple");
-    std::cout << "[DynamicArrays]: ____" << std::endl;
-    DynamicArray d_arr;
+class vec3{
+    float x, y, z;
+};
 
-    // d_arr.push_back(a);
+int main(){
+    typedef vec3 T1;
+    DynamicArray<T1> d1_arr(3);
+    d1_arr.push_back(T1());
+    d1_arr.push_back(T1());
+    d1_arr.push_back(T1());
+    d1_arr.push_back(T1());
+    d1_arr.push_back(T1());
+
+    typedef std::string T2;
+    DynamicArray<T2> d2_arr(5);
+    d2_arr.push_back(T2("Test"));
+    d2_arr.push_back(T2("Test"));
+    d2_arr.push_back(T2("Test"));
+    d2_arr.push_back(T2("Test"));
+    d2_arr.push_back(T2("Test"));
+    d2_arr.push_back(T2("Test"));
+    d2_arr.push_back(T2("Test"));
+
+    typedef myString T3;
+    DynamicArray<T3> d3_arr(5);
+    d3_arr.push_back(T3("Custom"));
+    d3_arr.push_back(T3("Custom"));
+    d3_arr.push_back(T3("Custom"));
+    d3_arr.push_back(T3("Custom"));
+    d3_arr.push_back(T3("Custom"));
+    d3_arr.push_back(T3("Custom"));
+    d3_arr.push_back(T3("Custom"));
+
 return 0;
 }
--- cpp-basic/src/cherno-yt/dynamic-arrays/dynaimc-array.hpp
+++ cpp-basic/src/cherno-yt/dynamic-arrays/dynaimc-array.hpp
@@ -5,29 +5,29 @@
 #include "../custom-strings/mystring.hpp"
 
 
-typedef myString T;
+// typedef myString T;
 // typedef std::string T;
+template <typename T>
 class DynamicArray{
 private:
     size_t n;
     size_t capacity;
-    T* resize();
-    void push_back();
+    T** resize();
     T at(size_t index);
-    T* T_objects = nullptr;
+    T** T_objects = new T*[0];
 
     // returns new location if succesful and nullptr if allocation wasn't succesful
-    T* resize(size_t new_capacity){
+    T** resize(size_t new_capacity){
         std::cout << "resize: Resizing from " << capacity << " to " << new_capacity << "." << std::endl;
 
-        T* newBuffer = new T[new_capacity];
+        T** newT_objects = new T*[new_capacity];
         for (size_t i=0; i<(new_capacity?new_capacity<capacity:capacity); ++i){
             //need to invoke copy constructor
-            newBuffer[i] = std::move(T_objects[i]);
+            newT_objects[i] = std::move(T_objects[i]);
         }
         capacity = new_capacity;
         delete[] T_objects;
-        T_objects = newBuffer;
+        T_objects = newT_objects;
         std::cout << "resize: Resized." << std::endl;
         return T_objects;
     }
@@ -38,26 +38,26 @@ public:
         this->capacity = size;
         this->n = 0;
         // Heap Allocation
-        T_objects = new T[capacity];
+        delete[] T_objects;
+        T_objects = new T*[capacity];
     }
 
     DynamicArray(size_t size, T _default):DynamicArray(size){
         std::cout<< "DynamicArray: Initialized array with value: " << _default << std::endl;
         for (size_t i = 0; i < size; i++){
-            // T_objects[i] = _default;
             this->push_back(_default);
         }
     }
 
     ~DynamicArray(){
         std::cout<< "~DynamicArray: Calling destructor." << std::endl;
-        delete[] T_objects;//, capacity;
+        delete[] T_objects;
     };
 
     //To access the data
     const T& operator[](size_t index) const{
         if (index<n)
-            return T_objects[index];
+            return *(T_objects[index]);
         else
             throw std::invalid_argument("Invalid index size");
     }
@@ -65,17 +65,18 @@ public:
     //To modify data
     T& operator[](size_t index){
         if (index<n)
-            return T_objects[index];
+            return *(T_objects[index]);
         else
             throw std::invalid_argument("Invalid index size");
     }
 
-    void push_back(const T& value){
+    void push_back(const T value){
         if (n == capacity){
             // resize the array and copy to new place
             this->resize((this->capacity+1)*3/2);
         }
-        this->T_objects[n] = value;
+        std::cout << "[push_back]:Pushing back (at index " << n << ")." << std::endl;
+        this->T_objects[n] = new T(value);
         n++;
     }
 
