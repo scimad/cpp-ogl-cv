42d81bcce2bb48197c3131b598c255f66c66787b
Madhav Humagain <scimad2.71@gmail.com>
2022-10-10 13:22:15 +0545

    Improve custom new-delete functionalities

--- cpp-basic/src/cherno-yt/custom-alloc/custom-alloc.hpp
+++ cpp-basic/src/cherno-yt/custom-alloc/custom-alloc.hpp
@@ -1,29 +1,38 @@
 #ifndef __CUSTOM_ALLOC__
-# define __CUSTOM_ALLOC__
+#define __CUSTOM_ALLOC__
 
 #include <stddef.h>
 #include <stdlib.h>
 #include <iostream>
 
+namespace zr
+{
+    bool custom_alloc_verbose = true;
+} // namespace zr
+
 void* operator new(size_t size){
     void* ptr = malloc(size);
-    std::cout << "[new]: [" << size << " Bytes] allocated on heap at location:" << ptr << std::endl;
+    if (zr::custom_alloc_verbose == true)
+        std::cout << "[new]: [" << size << " Bytes] allocated on heap at location:" << ptr << std::endl;
     return ptr;
 }
 
 void operator delete(void* ptr){
     free(ptr);
-    std::cout << "[delete]: Memory freed from heap at location:" << ptr << std::endl;
+    if (zr::custom_alloc_verbose == true)
+        std::cout << "[delete]: Memory freed from heap at location:" << ptr << std::endl;
 }
 
 void* operator new[](size_t size){
     void* ptr = malloc(size);
-    std::cout << "[new[]]: [" << size << " Bytes] allocated on heap at location:" << ptr << std::endl;
+    if (zr::custom_alloc_verbose == true)
+        std::cout << "[new[]]: [" << size << " Bytes] allocated on heap at location:" << ptr << std::endl;
     return ptr;
 }
 
 void operator delete[](void* ptr, size_t size){
-    std::cout << "[delete[]]: Freeing up heap memory at location:" << ptr << std::endl;
+    if (zr::custom_alloc_verbose == true)
+        std::cout << "[delete[]]: Freeing up heap memory at location:" << ptr << std::endl;
     free(ptr);
 }
 
